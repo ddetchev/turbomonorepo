725bc805b8b09d9b9205710e1c15b1411e100f07
Daniel Detchev <white_daniell@berkeley.edu>
2024-01-11 00:59:38 -0800

    added x icon to bottom nav

--- apps/web/src/app/ui/popupnav.tsx
+++ apps/web/src/app/ui/popupnav.tsx
@@ -24,11 +24,21 @@ export default function Popup() {
 <div className="popuppos">
 
 <button onClick={() => {canSee ? setSee(false) : setSee(true)}}>
-<div className="bg-zinc-300 p-2 rounded-full">
-<svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
+  {canSee ? 
+  <div className="bg-zinc-200 p-2 rounded-full">
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
 </svg>
 </div>
+  
+  : 
+  
+  <div className="bg-zinc-300 p-2 rounded-full">
+   <svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" /></svg> 
+</div>
+  }
+
 </button>
 
 
