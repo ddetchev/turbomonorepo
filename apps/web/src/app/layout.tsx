d6bbddedf1e9d1a110a4a3e61f900abbcbdd0c6a
Daniel Detchev <white_daniell@berkeley.edu>
2024-01-17 10:47:47 -0800

    added back in style import fixed import in docs app

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,5 @@
 // import "./globals.css";
-// import "@repo/ui/styles";
+import "@repo/ui/styles";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "./ui/bottomnav";
